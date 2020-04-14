0ead675e7ec34ad55b9c6db893d6814be829a447
Calix Huang <calix.huang1@gmail.com>
2020-04-13 21:50:11 -0700

    media capture changes by ellen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {Media,MediaObject} from "@ionic-native/media/";
+import {File} from "@ionic-native/file/";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
+export class HomePage {
+  status: string = "";
+  audioFile: MediaObject = this.media.create(this.file.externalRootDirectory + '/audiofile.mp3');
+
+  constructor(private media: Media, private file: File) {}
+
+  RecordAudio() {
+    this.audioFile.startRecord();
+    this.status = "recording..."
+  }
+
+  StopRecording() {
+    this.audioFile.stopRecord();
+    this.status = "stopped recording";
+  }
+
 export class HomePage implements OnInit {
 
   constructor() { }
